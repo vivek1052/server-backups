2220da87328dd198cbf730ca5b84b27b809ad36b
Vivek Parashar <vivek.parashar@shell.com>
2024-02-28 23:39:38 +0530

    pg dump all

--- immich/backup.sh
+++ immich/backup.sh
@@ -1,3 +1,12 @@
 #!/bin/sh
 
-echo "$POSTGRES_HOST Starting backup"
+echo "Starting backup at $(TZ=$TZ date)"
+
+mkdir -p /backup/immich-borg
+
+mkdir -p /upload/database-backup
+
+borg init --encryption=none /backup/immich-borg
+
+pg_dumpall -h $POSTGRES_HOST -c -U $POSTGRES_USER | gzip > /upload/database-backup/immich-database.sql.gz
+

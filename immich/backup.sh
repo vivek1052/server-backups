8352c885b757eeeb3c343565eecac64b37d7b94b
Vivek Parashar <vivek.parashar@shell.com>
2024-02-29 14:08:17 +0530

    borg backup

--- immich/backup.sh
+++ immich/backup.sh
@@ -2,13 +2,30 @@
 
 echo "Starting backup at $(TZ=$TZ date)"
 
-mkdir -p /backup/immich-borg
+mkdir -p "$BACKUP_PATH/immich-borg"
 
-mkdir -p /upload/database-backup
+mkdir -p "$UPLOAD_LOCATION/database-backup"
 
-borg init --encryption=none /backup/immich-borg
+borg init --encryption=none "$BACKUP_PATH/immich-borg"
 
 export PGPASSWORD=$POSTGRES_PASSWORD
 
-pg_dumpall -h $POSTGRES_HOST -p $POSTGRES_PORT -c -U $POSTGRES_USER | gzip > /upload/database-backup/immich-database.sql.gz
+echo "Starting database dump"
 
+pg_dumpall -h $POSTGRES_HOST -p $POSTGRES_PORT -c -U $POSTGRES_USER | gzip > $UPLOAD_LOCATION/database-backup/immich-database.sql.gz
+
+echo "Database dump complete"
+
+echo "Starting borg backup"
+
+borg create --stats $BACKUP_PATH/immich-borg::{now} $UPLOAD_LOCATION --exclude $UPLOAD_LOCATION/thumbs/ --exclude $UPLOAD_LOCATION/encoded-video/
+
+echo "Pruning old backups"
+
+borg prune --keep-weekly=4 --keep-monthly=3 $BACKUP_PATH/immich-borg
+
+echo "Compacting backup"
+
+borg compact $BACKUP_PATH/immich-borg
+
+echo "Borg backup complete"

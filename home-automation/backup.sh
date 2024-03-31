d4350e156e3060a8ea312e934bb81bed72293027
Vivek <vivekparashar811@gmail.com>
2024-03-31 23:45:05 +0530

    homeautomation script

---
+++ home-automation/backup.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+echo "Starting backup at $(date)"
+
+mkdir -p "$BACKUP_PATH/ha-borg"
+
+mkdir -p $BORG_DATA_PATH
+
+export BORG_UNKNOWN_UNENCRYPTED_REPO_ACCESS_IS_OK=yes
+
+export BORG_BASE_DIR=$BORG_DATA_PATH
+
+borg init --encryption=none "$BACKUP_PATH/ha-borg"
+
+echo "Starting borg backup"
+
+borg create --stats $BACKUP_PATH/ha-borg::{now} $UPLOAD_LOCATION
+
+echo "Pruning old backups"
+
+borg prune --keep-weekly=4 --keep-monthly=3 $BACKUP_PATH/ha-borg
+
+echo "Compacting backup"
+
+borg compact $BACKUP_PATH/ha-borg
+
+echo "Borg backup complete"

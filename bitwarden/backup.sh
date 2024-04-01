ae19fe4457cb5839172407cbafe63ede52875cbe
Vivek Parashar <vivek.parashar@shell.com>
2024-04-01 16:28:46 +0530

    bitwarden use resuable image

--- bitwarden/backup.sh
+++ bitwarden/backup.sh
@@ -4,12 +4,6 @@ echo "Starting backup at $(date)"
 
 mkdir -p "$BACKUP_PATH/bitwarden-borg"
 
-mkdir -p $BORG_DATA_PATH
-
-export BORG_UNKNOWN_UNENCRYPTED_REPO_ACCESS_IS_OK=yes
-
-export BORG_BASE_DIR=$BORG_DATA_PATH
-
 borg init --encryption=none "$BACKUP_PATH/bitwarden-borg"
 
 echo "Starting borg backup"
@@ -18,7 +12,7 @@ borg create --stats $BACKUP_PATH/bitwarden-borg::{now} $UPLOAD_LOCATION
 
 echo "Pruning old backups"
 
-borg prune --keep-weekly=4 --keep-monthly=3 $BACKUP_PATH/bitwarden-borg
+borg prune $BORG_PRUNE_OPTIONS $BACKUP_PATH/bitwarden-borg
 
 echo "Compacting backup"
 

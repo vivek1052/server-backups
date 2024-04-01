645b597be3ef8cfc5e3ca520e14dd0a3ba593f5e
Vivek Parashar <vivek.parashar@shell.com>
2024-04-01 15:29:45 +0530

    pass env directly to cron job

--- init.sh
+++ init.sh
@@ -5,12 +5,8 @@ echo "Running custom script with cron schedule $CRON_TIME"
 echo "Creating upload and backup directories"
 mkdir -p $BACKUP_PATH $UPLOAD_LOCATION
 
-echo "Setting Borg environment variables"
-export BORG_UNKNOWN_UNENCRYPTED_REPO_ACCESS_IS_OK=yes
-export BORG_BASE_DIR=/borg-data
-
 echo "Downloading backup script"
 wget $BORG_BACKUP_SCRIPT -O /backup-script && chmod +x /backup-script
 
 echo "Scheduling backup script"
-echo "$CRON_TIME /backup-script" > /etc/crontabs/root
+echo "$CRON_TIME BORG_UNKNOWN_UNENCRYPTED_REPO_ACCESS_IS_OK=yes BORG_BASE_DIR=/borg-data /backup-script" > /etc/crontabs/root

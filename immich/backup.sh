c1b326ab022825d9b657903d1623e76606d0860b
Vivek Parashar <vivek.parashar@shell.com>
2024-04-01 17:35:47 +0530

    dependiencies

--- immich/backup.sh
+++ immich/backup.sh
@@ -10,6 +10,10 @@ borg init --encryption=none "$BACKUP_PATH/immich-borg"
 
 export PGPASSWORD=$POSTGRES_PASSWORD
 
+echo "Installing dependencies"
+
+apk add postgresql14-client gzip -y
+
 echo "Starting database dump"
 
 pg_dumpall -h $POSTGRES_HOST -p $POSTGRES_PORT -c -U $POSTGRES_USER | gzip > $UPLOAD_LOCATION/database-backup/immich-database.sql.gz

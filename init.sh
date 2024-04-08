05d632beaefb6bba525bd1162bfa81614774abc8
Vivek Parashar <vivek.parashar@shell.com>
2024-04-08 19:17:41 +0530

    BORG_RELOCATED_REPO_ACCESS_IS_OK=yes

--- init.sh
+++ init.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 echo "Scheduling backup script with schedule $CRON_TIME"
-echo "$CRON_TIME BORG_UNKNOWN_UNENCRYPTED_REPO_ACCESS_IS_OK=yes BORG_BASE_DIR=/borg-data /backup-script" > /etc/crontabs/root
+echo "$CRON_TIME BORG_UNKNOWN_UNENCRYPTED_REPO_ACCESS_IS_OK=yes BORG_RELOCATED_REPO_ACCESS_IS_OK=yes BORG_BASE_DIR=/borg-data /backup-script" > /etc/crontabs/root
